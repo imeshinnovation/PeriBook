8eaaf61803b47ed2d7cddfef0ab657d306230451
PeriBook Dev <dev@peribook.local>
2026-07-24 06:34:00 +0000

    Update last preview delivery

--- user-service/src/main/java/com/peribook/user/infrastructure/OpenApiConfig.java
+++ user-service/src/main/java/com/peribook/user/infrastructure/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.peribook.user.infrastructure;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +22,13 @@ public class OpenApiConfig {
                         .description("Perfiles de usuario")
                         .version("1.0.0"))
                 .servers(List.of(
-                        new Server().url("http://192.168.50.31:8080").description("Gateway - produccion")));
+                        new Server().url("http://192.168.50.31:8080").description("Gateway - produccion")))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Token JWT obtenido en auth-service")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
